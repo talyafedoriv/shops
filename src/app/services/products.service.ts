eb34765c0cbe2a331b52ce91c9ae1ce878b7e3fb
talyafedoriv <115717490+talyafedoriv@users.noreply.github.com>
2023-12-23 15:55:19 +0200

    Update products.service.ts

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -7,7 +7,7 @@ import { Observable } from "rxjs";
 })
 
 export class ProductsService {
-    private apiUrl = 'https://talyafedoriv.github.io/shops/db.json';
+    private apiUrl = 'https://talyafedoriv.github.io/json/db.json';
   
     constructor(private http: HttpClient) { 
 
